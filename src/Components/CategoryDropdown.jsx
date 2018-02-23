b91ce13ae7a7954d22eeaaf8a1b6da734c29c9b9
DanteUh <dante.uhlinder@gmail.com>
2018-02-23 17:31:28 +0100

    Fetch data and send it to table - Work in progress

--- src/Components/CategoryDropdown.jsx
+++ src/Components/CategoryDropdown.jsx
@@ -3,9 +3,23 @@ import { Collapse } from 'reactstrap';
 import DataTable from './DataTable';
 
 export default class CategoryDropdown extends Component {
+  componentDidMount() {
+    this.getCharacterData();
+
+  };
+  
   state = {
     collapse: true,
-    characterData: 'characterData',
+    characterData: [],
+  }
+
+  getCharacterData = () => {
+    fetch('http://localhost:3000/Server/CharacterData/lars.json')
+      .then(res => res.json())
+      .then(response =>
+        this.setState({ characterData: response })
+      )
+      .catch(error => console.log(error));
   }
 
   toggleClick = () => {
@@ -13,6 +27,7 @@ export default class CategoryDropdown extends Component {
   }
 
   render() {
+    console.log(this.state.characterData);
     return (
       <div className="category-dropdown pt-5 pb-3">
         <button className="dropdown-btn" onClick={this.toggleClick}>
--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -7,37 +7,36 @@ const DataTable = props => (
     <ReactTable
       columns={[
         {
-          Header: "Command",
+          Header: 'Command',
         },
         {
-          Header: "Hit Level",
+          Header: 'Hit Level',
         },
         {
-          Header: "Damage",
+          Header: 'Damage',
         },
         {
-          Header: "Start Up Frame",
+          Header: 'Start Up Frame',
         },
         {
-          Header: "Block Frame",
+          Header: 'Block Frame',
         },
         {
-          Header: "Hit Frame",
+          Header: 'Hit Frame',
         },
         {
-          Header: "Counter Hit Frame",
+          Header: 'Counter Hit Frame',
         },
         {
-          Header: "Notes",
+          Header: 'Notes',
         },
       ]}
     />
-    <h2>{props.characterData}</h2>
   </div>
 );
 
 DataTable.propTypes = {
-  characterData: PropTypes.string.isRequired,
+  
 };
 
 export default DataTable;
