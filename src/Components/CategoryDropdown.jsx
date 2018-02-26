c7ee5a5db73ae14bfffea235b1f61f8e2b867d0a
DanteUh <dante.uhlinder@gmail.com>
2018-02-26 22:06:45 +0100

    Add data to table

--- src/Components/CategoryDropdown.jsx
+++ src/Components/CategoryDropdown.jsx
@@ -3,23 +3,8 @@ import { Collapse } from 'reactstrap';
 import DataTable from './DataTable';
 
 export default class CategoryDropdown extends Component {
-  componentDidMount() {
-    this.getCharacterData();
-
-  };
-  
   state = {
     collapse: true,
-    characterData: [],
-  }
-
-  getCharacterData = () => {
-    fetch('http://localhost:3000/Server/CharacterData/lars.json')
-      .then(res => res.json())
-      .then(response =>
-        this.setState({ characterData: response })
-      )
-      .catch(error => console.log(error));
   }
 
   toggleClick = () => {
@@ -27,14 +12,13 @@ export default class CategoryDropdown extends Component {
   }
 
   render() {
-    console.log(this.state.characterData);
     return (
       <div className="category-dropdown pt-5 pb-3">
         <button className="dropdown-btn" onClick={this.toggleClick}>
           <h2>Category Dropdown</h2>
         </button>
         <Collapse isOpen={this.state.collapse}>
-          <DataTable characterData={this.state.characterData} />
+          <DataTable />
         </Collapse>
       </div>
     );
--- src/Components/DataTable.jsx
+++ src/Components/DataTable.jsx
@@ -1,42 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
+import LarsData from '../Server/CharacterData/lars.json';
 
-const DataTable = props => (
-  <div className="data-table">
-    <ReactTable
-      columns={[
-        {
-          Header: 'Command',
-        },
-        {
-          Header: 'Hit Level',
-        },
-        {
-          Header: 'Damage',
-        },
-        {
-          Header: 'Start Up Frame',
-        },
-        {
-          Header: 'Block Frame',
-        },
-        {
-          Header: 'Hit Frame',
-        },
-        {
-          Header: 'Counter Hit Frame',
-        },
-        {
-          Header: 'Notes',
-        },
-      ]}
-    />
-  </div>
-);
-
-DataTable.propTypes = {
+export default class DataTable extends Component {
+  state = {
+    frameData: LarsData.moves,
+  }
   
+  render() {
+    console.log(this.state.frameData);
+    return(
+      <div className="data-table">
+        <ReactTable
+          columns={[
+            {
+              Header: 'Command',
+              accessor: 'command',
+            },
+            {
+              Header: 'Hit Level',
+              accessor: 'hitLevel',
+            },
+            {
+              Header: 'Damage',
+              accessor: 'damage',
+            },
+            {
+              Header: 'Start Up Frame',
+              accessor: 'startUpFrame',
+            },
+            {
+              Header: 'Block Frame',
+              accessor: 'blockFrame',
+            },
+            {
+              Header: 'Hit Frame',
+              accessor: 'hitFrame',
+            },
+            {
+              Header: 'Counter Hit Frame',
+              accessor: 'counterHitFrame',
+            },
+            {
+              Header: 'Notes',
+              accessor: 'notes',
+            },
+          ]}
+          data={this.state.frameData}
+          className="-striped -highlight"
+        />
+      </div>
+    );  
+  }
 };
 
-export default DataTable;
