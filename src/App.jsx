087118a79e3da7e3e09339a0b5062adbf80979d3
DanteUh <dante.uhlinder@gmail.com>
2018-02-27 16:19:00 +0100

    Add character navigation and aligning components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { ListGroup, Container, Row, Col } from 'reactstrap';
 import './App.css';
-
+import characters from './Server/characterNames';
 import DataContainer from './Containers/DataContainer';
+import CharacterList from './Components/CharacterList';
 
 class App extends Component {
   state = {
-    error: false
+    characterList: characters.characterNames,
+    selectedCharacter: '',
   }
 
+  handleClick = (e) => {this.setState({ [e.target.name]: e.target.value })};
+
   render() {
+
+    const characterNavigation = this.state.characterList.map((char, i) => {
+      return (
+        <CharacterList
+          key={i}
+          handleClick={this.handleClick}
+          characterName={char} />
+      );
+    });
+    console.log(this.state.selectedCharacter);
     return (
       <div className="App">
-        <DataContainer />
+        <Container style={{ maxWidth: "97%" }}>
+          <Row>
+            <Col xs="2" style={{ padding: 0 }}>
+              <div className="character-nav">
+                <ListGroup>
+                  {characterNavigation}
+                </ListGroup>
+              </div>
+            </Col>
+            <Col style={{ padding: 0 }}>
+              <DataContainer
+                selectedCharacter={this.state.selectedCharacter} />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
