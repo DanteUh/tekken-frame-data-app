e975c27f4020e5b3af22a3c8f551afcfdde612d9
dulinder <dante.ulinder@chas.se>
2018-03-20 17:39:46 +0100

    DataContainer, styling & html alignment

--- src/Components/CategoryDropdown.jsx
+++ src/Components/CategoryDropdown.jsx
@@ -15,11 +15,11 @@ export default class CategoryDropdown extends Component {
 
   render() {
     return (
-      <div>
+      <div className="category-container m-3">
         {this.props.characterData[0] !== undefined &&
         <div className="category-dropdown p-3">
           <button className="dropdown-btn" onClick={this.toggleClick}>
-            <h4>{`${this.props.characterData[0].type}s`}</h4>
+            {`${this.props.characterData[0].type}s`}
           </button>
           <Collapse isOpen={this.state.collapse}>
             <DataTable characterData={this.props.characterData} />
