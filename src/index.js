115977d6c53efbf950e86ad29e5adc6f60f9c915
Dante Ulinder <dante.uhlinder@gmail.com>
2018-03-20 13:11:50 +0100

    Feature/style (#15)
    * Add sass watch css, restructuring css, npm-run-all

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'react-table/react-table.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
