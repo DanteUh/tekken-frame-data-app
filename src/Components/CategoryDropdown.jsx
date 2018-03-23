81eb732a7f3ab12f4eb3f4f29e4fba35b7d605a7
Dante Ulinder <dante.uhlinder@gmail.com>
2018-03-23 13:22:27 +0100

    Dropdown icons, switch on toggle (#21)

--- src/Components/CategoryDropdown.jsx
+++ src/Components/CategoryDropdown.jsx
@@ -18,8 +18,12 @@ export default class CategoryDropdown extends Component {
       <div className="category-container">
         {this.props.characterData[0] !== undefined &&
         <div className="category-dropdown mb-5">
-          <button className="btn-custom dropdown" onClick={this.toggleClick}>
+          <button className="btn-custom dropdown d-flex justify-content-between align-items-center" onClick={this.toggleClick}>
             {`${this.props.characterData[0].type}s`}
+            {this.state.collapse ?
+              <i className="fa fa-angle-up fa-2x" aria-hidden="true"></i> :
+              <i className="fa fa-angle-down fa-2x" aria-hidden="true"></i>
+            }
           </button>
           <Collapse isOpen={this.state.collapse}>
             <DataTable characterData={this.props.characterData} />
