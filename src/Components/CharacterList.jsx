caff288003e275e8930125e936a5d0d05575c728
Dante Ulinder <dante.uhlinder@gmail.com>
2018-03-28 12:53:18 +0200

    Data and display name updates correctly on character menu click (#25)

--- src/Components/CharacterList.jsx
+++ src/Components/CharacterList.jsx
@@ -5,18 +5,22 @@ import { DropdownItem } from 'reactstrap';
 const CharacterList = props => (
   <DropdownItem
     onClick={props.handleChange}
-    value={props.selectedCharacter}
+    value={props.characterNameValue}
     name="selectedCharacter"
     className="menu-item d-flex align-items-center"
   >
-    <img src={props.characterThumbnail} alt="" className="character-thumbnail mr-3" />
-    <h4>{props.characterName}</h4>
+    <img
+      src={props.characterThumbnail} 
+      alt="character-thumbnail"
+      className="character-thumbnail mr-3"
+    />
+    {props.characterName}
   </DropdownItem>
 );
 
 CharacterList.propTypes = {
   characterName: PropTypes.string.isRequired,
-  selectedCharacter: PropTypes.string.isRequired,
+  characterNameValue: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
   characterThumbnail: PropTypes.string.isRequired,
 };
