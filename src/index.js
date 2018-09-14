bbdb83db5983664985cbd6176d0a5fb6329812e3
Dante Ulinder <dante.uhlinder@gmail.com>
2018-09-14 10:11:11 +0200

    Change location, names of files and css classes (#34)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import App from './App';
+import App from './Components/Main';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
